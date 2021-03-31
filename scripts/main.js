713f1e5f009a0fed4094cf85847148e4e2d82dc8
isrameve <israelmedive@gmail.com>
2021-03-30 20:19:57 -0500

    done weater API in the header...

--- scripts/main.js
+++ scripts/main.js
@@ -9,8 +9,7 @@ $(document).ready(function () {
      });
 
      //Getting the API
-     const getApi =
-          "https://api.openweathermap.org/data/2.5/weather?id=3674962&appid=62120a81c75c69d7134fb99e04276204&lang=es&units=metric";
+     const getApi ="https://api.openweathermap.org/data/2.5/weather?id=3674962&appid=62120a81c75c69d7134fb99e04276204&lang=es&units=metric";
      const toPrint = document.querySelector('#weather');
 
      function weather() {
@@ -18,9 +17,29 @@ $(document).ready(function () {
                .then((response) => response.json())
                .then((data) => {
                     console.log("Perfect", data);
-                    //toPrint.innerHTML = `Hello`; Aqui va lo que se va a imprimir
+                    toPrint.innerHTML = `
+                                        <div class="row container-weather">
+                                             <div class="col-12 city text-align-c margin-botton">
+                                                  <p>Clima en: Medellín</p>
+                                             </div>
+                                             <div class="row cont-temp-desc">
+                                                  <div class="col-6 temp">
+                                                       <p>Temperatura:</p>
+                                                       <p>${data.main.temp}ºC</p>
+                                                  </div>
+                                             <div class="col-6 description">
+                                                       <p>description:</p>
+                                                       <p>${data.weather[0].description}</p>
+                                             </div>
+                                        </div>
+                                        `; 
                })
                .catch("Algo aslió mal");
-     } //Consumiendo la API
+     }
+
+     function news() {
+
+     }
+     news();
      weather();
 });
